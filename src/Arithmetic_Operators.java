07cd02970bb5267b53da3ea3041edaceb64599ee
Sumit Shegokar <sumitshegokar46@gmail.com>
2025-05-20 22:10:24 +0530

    Java_Opretors

---
+++ src/Arithmetic_Operators.java
@@ -0,0 +1,14 @@
+public class Arithmetic_Operators {
+
+    public static void main(String[] args) {
+
+        int a=50;
+        int b=10;
+
+        System.out.println("Addition of two Number :" + (a+b));
+        System.out.println("Subtraction of two Number :" + (a-b));
+        System.out.println("Multiplication of two Number :" + (a*b));
+        System.out.println("Division of two Number :" + (a/b));
+        System.out.println("Modulus of two Number :" + (a % b));
+    }
+}
---
+++ src/Assigment_Operators.java
@@ -0,0 +1,9 @@
+public class Assigment_Operators {
+
+    public static void main(String[] args) {
+
+        int age=55;
+
+        System.out.println("Assigment Operator Value : "+ age);
+    }
+}
