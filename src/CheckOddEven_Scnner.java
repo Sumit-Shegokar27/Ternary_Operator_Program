1678f3f3b91e312401d130fd7b2d48da8c1c2bb8
Sumit Shegokar <sumitshegokar46@gmail.com>
2025-05-20 22:40:00 +0530

    Check_ODD_EVEN_NUMBER_Using Scanner Function

---
+++ src/CheckOddEven_Scnner.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CheckOddEven_Scnner {
+
+    public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Enter the Number :");
+
+        int number= sc.nextInt();
+
+        String result=(number % 2==0) ? "Even" : "Odd";
+        System.out.println(number + " is " + result );
+        sc.close();
+    }
+}
